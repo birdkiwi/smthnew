8918d212fed877a9600871d59832de43a2914c02
Alexey Toklovich <alexey@toklovich.com>
2015-04-07 13:44:24 +0600

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,73 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        less: {
+            frontend: {
+                options: {
+                    paths: ["css"]
+                },
+                files: {"build/style.css": "less/style.less"}
+            }
+        },
+
+        cssmin: {
+            combine: {
+                files: {
+                    'build/style.min.css': ['build/style.css']
+                }
+            }
+        },
+
+        concat: {
+            dist: {
+                src: [
+                    'bower_components/jquery/dist/jquery.js',
+                    'bower_components/bootstrap/dist/js/bootstrap.js',
+                    'bower_components/jquery-validate/dist/jquery.validate.js',
+                    'js/bg-loaded.js',
+                    'js/script.js'
+                ],
+                dest: 'build/script.js'
+            }
+        },
+
+        uglify: {
+            build: {
+                src: 'build/script.js',
+                dest: 'build/script.min.js'
+            }
+        },
+
+        sprite:{
+            less: {
+                src: 'img/sprites/*.png',
+                dest: 'build/spritesheet.png',
+                destCss: 'build/sprites-vars.less'
+            }
+        },
+
+        watch: {
+            frontend: {
+                files: ['js/**/*.js', 'images/sprites/*', 'less/**/*.less'],
+                tasks: ['sprite', 'less', 'cssmin', 'concat', 'uglify'],
+                options: {
+                    livereload: true,
+                    spawn: false
+                }
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-spritesmith');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['sprite', 'less', 'cssmin', 'concat', 'uglify', 'watch']);
+
+};
---
+++ js/script.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+    $(".js-validate").each(function(){
+        $(this).validate({
+            errorPlacement: function(error, element) {},
+            highlight: function(element, errorClass, validClass) {
+                if ($(element).parent().hasClass('form-control')) {
+                    $(element).parent().addClass(errorClass).removeClass(validClass);
+                } else {
+                    $(element).addClass(errorClass).removeClass(validClass);
+                }
+            },
+            unhighlight: function(element, errorClass, validClass) {
+                if ($(element).parent().hasClass('form-control')) {
+                    $(element).parent().removeClass(errorClass).addClass(validClass);
+                } else {
+                    $(element).removeClass(errorClass).addClass(validClass);
+                }
+            }
+        });
+    });
+
+    $('.welcome-block-iphone').bgLoaded({
+        afterLoaded : function() {
+            $(this).addClass('animated bounceInDown');
+        }
+    });
+
+    $('.form-control > input, .form-control > textarea').on("focus blur", function() {
+        if ($(this).is(":focus")) {
+            $(this).parent().addClass('has-focus');
+        } else {
+            $(this).parent().removeClass('has-focus');
+        }
+    });
+
+    $('.js-custom-file-input').on("focus blur change", function() {
+        var filename = $(this).val();
+        var title = $(this).siblings('.input-file-title').data('custom-input-file-default-title');
+        var subtitle = $(this).siblings('.input-file-subtitle');
+
+        if (filename != '') {
+            if (filename.length > 25) {
+                filename = filename.substr(0,25) + '...';
+            }
+            $(this).siblings('.input-file-title').html(filename);
+            subtitle.animate({opacity: 0}, 300);
+        } else {
+            $(this).siblings('.input-file-title').html(title);
+            subtitle.animate({opacity: 1}, 300);
+        }
+    });
+});
